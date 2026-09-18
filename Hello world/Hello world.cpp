4ac70e7ed160cb927fa40d4b5fd79d3a3f91d955
David Cole <mdcgr33n@gmail.com>
2026-09-18 17:33:55 -0600

    Add project files.

---
+++ Hello world/Hello world.cpp
@@ -0,0 +1,9 @@
+// includes: iostream enables inputs and outputs to the consol
+#include <iostream>
+
+// main function. In this simple program it is the only function and it displays a string output to the consol
+int main()
+{
+    // the string that is to be outputted to the consol
+    std::cout << "Hello World!\n";
+}
