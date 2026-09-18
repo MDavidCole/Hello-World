3650f723c4644aebc6b27fe9c55dbdb0cd2aba3c
David Cole <mdcgr33n@gmail.com>
2026-09-18 17:56:38 -0600

    fixed misspellings in comments

--- Hello world/Hello world.cpp
+++ Hello world/Hello world.cpp
@@ -1,9 +1,9 @@
-// includes: iostream enables inputs and outputs to the consol
+// includes: iostream enables inputs and outputs to the console
 #include <iostream>
 
-// main function. In this simple program it is the only function and it displays a string output to the consol
+// main function. In this simple program it is the only function and it displays a string output to the console
 int main()
 {
-    // the string that is to be outputted to the consol
+    // the string that is to be outputted to the console
     std::cout << "Hello World!\n";
 }
